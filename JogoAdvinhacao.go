89714fd6675634b1dfc9e356065b541cf4608d4d
PedroDiOliveira <pedro.h.soterio@gmail.com>
2023-05-19 17:58:21 -0300

    done

--- JogoAdvinhacao.go
+++ JogoAdvinhacao.go
@@ -11,32 +11,55 @@ func main() {
 	fmt.Println("|--Bem-vindo ao jogo da adivinhação!--|")
 	fmt.Println("|-------------------------------------|")
 
-	rand.Seed(time.Now().UnixNano())
-	numeroAleatorio := rand.Intn(100) + 1
-
 	//Declaracao de variaveis
 	var resposta int
 	var contador int
+	var jogar bool
+	jogar = true
+	var x int
+	var slice []int
+	for jogar == true {
+		rand.Seed(time.Now().UnixNano())
+		numeroAleatorio := rand.Intn(100) + 1
+		for resposta != numeroAleatorio {
+			contador++
+			fmt.Println(numeroAleatorio)
+			fmt.Println(contador, "º tentativa")
+			fmt.Println("Digite um numero aleatorio entre 1 e 100: ")
+			fmt.Scan(&resposta)
+			fmt.Println("**************************")
+			if resposta == numeroAleatorio {
+				fmt.Println("Parabens voce acertou!", numeroAleatorio, "era o numero aleatorio. :)")
+				fmt.Println("Voce usou", contador, "tentativas!")
+				break
+			} else if resposta > 100 || resposta < 1 {
+				fmt.Println("O numero deve estar entre 1 e 100")
+			} else if resposta > numeroAleatorio {
+				fmt.Println("O numero é MENOR que", resposta)
+				fmt.Println("**************************")
+			} else if resposta < numeroAleatorio {
+				fmt.Println("O numero é MAIOR que ", resposta)
+				fmt.Println("**************************")
+			}
+		}
+		for i := 0; i < 1; i++ {
+			slice = append(slice, contador)
+		}
 
-	for resposta != numeroAleatorio {
-		contador++
-		fmt.Println(numeroAleatorio)
-		fmt.Println("Tentativa de numero", contador)
-		fmt.Println("Digite um numero aleatorio entre 1 e 100: ")
-		fmt.Scan(&resposta)
-		fmt.Println("**************************")
-		if resposta == numeroAleatorio {
-			fmt.Println("Parabens voce acertou!", numeroAleatorio, "era o numero aleatorio. :)")
-			fmt.Printf("Voce usou %d tentativas", contador)
-			break
-		} else if resposta > 100 || resposta < 1 {
-			fmt.Println("O numero deve estar entre 1 e 100")
-		} else if resposta > numeroAleatorio {
-			fmt.Println("O numero é MENOR que", resposta)
+		fmt.Println("Quer jogar novamente? (1)Sim (2)Não")
+		fmt.Scan(&x)
+		contador = 0
+		if x == 1 {
 			fmt.Println("**************************")
-		} else if resposta < numeroAleatorio {
-			fmt.Println("O numero é MAIOR que ", resposta)
+			fmt.Println("Certo! Sorteando um novo numero...")
+		} else if x == 2 {
 			fmt.Println("**************************")
+			fmt.Println("Obrigado por jogar!")
+			for i := 0; i < len(slice); i++ {
+				fmt.Println("Na", i+1, "º rodada, voce utilizou de", slice[i])
+			}
+			jogar = false
+
 		}
 	}
 }
